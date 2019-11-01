a4905e9193374051141d0efdfc51f54f5f048794
Laura Collins <laura.e.collins1@gmail.com>
2019-11-01 18:22:16 -0500

    home is done

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,10 @@
 import 'bootstrap';
 import '../styles/main.scss';
-import myNavBar from './components/myNavBar/myNavBar';
+
 import home from './components/home/home';
 
 const init = () => {
-  myNavBar.printButton();
-  home.printMainPage();
+  home.printHome();
 };
 
 init();
