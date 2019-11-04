0e67469fcda144297b7035bf24d49cf1f848176f
Laura Collins <laura.e.collins1@gmail.com>
2019-11-03 23:42:22 -0600

    cards and pins are printing to page and change with user login

--- src/javascripts/components/singleBoard/singleBoard.js
+++ src/javascripts/components/singleBoard/singleBoard.js
@@ -1,33 +1,22 @@
 import './singleBoard.scss';
 import smash from '../../helpers/data/smash';
 import utilities from '../../helpers/utilities';
-import pinData from '../../helpers/data/pinData';
+import pins from '../pins/pins';
 
 const makeTheBoards = () => {
   smash.getCompleteBoards()
     .then((boards) => {
       let domString = '';
-      let domString2 = '';
       boards.forEach((board) => {
         domString += '<div class="card col-4">';
         domString += `<h5 class="card-title">${board.name}</h5>`;
+        domString += `<div id="${board.id}"></div>`;
         domString += `<div class="card-body">
                       <p class="card-text">${board.description}</p>
                     </div>`;
-        domString += `<div id="${board.id}"></div>`;
         domString += '</div>';
         utilities.printToDom('board-section', domString);
-        pinData.getPinData(board.id).then((pins) => {
-          pins.forEach((pin) => {
-            if (board.id === pin.boardId) {
-              domString2 += `
-              <img src=${pin.imageUrl} class="card-img-top" alt="...">`;
-              utilities.printToDom(`${pin.boardId}`, domString2);
-            } else {
-              domString2 += '';
-            }
-          });
-        });
+        pins.getMyPins(board.id);
       });
     })
     .catch((error) => console.error(error));
